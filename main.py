c433677c183027bcc8a54eb894ba502dd909d619
xmkxabc <6680886+xmkxabc@users.noreply.github.com>
2024-04-03 16:29:18 +0800

    Update main.py
    Get information from https://eprint.iacr.org/rss/atom.xml.

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 
 import requests
 
-from eprint_parser import EPrintParser
 from tweet import tweet
 from storage import Storage
-from config import HTTP_HEADERS, sentry_client
+from config import HTTP_HEADERS
+from paperscraper import PaperScraper
+from datetime import datetime
 
-
-@sentry_client.capture_exceptions
+# @sentry_client.capture_exceptions
 def main():
+#    now = datetime.now()
+#    print("the time: ", now)
     resp = requests.get(
-        'http://eprint.iacr.org/eprint-bin/search.pl?last=31&title=1',
+        'https://eprint.iacr.org/rss/atom.xml',
         headers=HTTP_HEADERS
     )
 
@@ -19,10 +21,14 @@ def main():
         msg = 'request failed: ' + str(resp.status_code) \
             + '\n\n' + resp.text
         raise Exception(msg)
+        
+    # my_parser = EPrintParser()
+    # curr_list = my_parser.feed(resp.text)
 
-    my_parser = EPrintParser()
-    curr_list = my_parser.feed(resp.text)
-
+    scraper = PaperScraper('https://eprint.iacr.org/rss/atom.xml')
+    curr_list = scraper.parse_data()
+#    now = datetime.now()
+#    print("the time: ", now)
     if curr_list is None \
             or not isinstance(curr_list, list) \
             or len(curr_list) < 20:
@@ -35,18 +41,20 @@ def main():
             or not isinstance(prev_list, list) \
             or len(prev_list) == 0:
         my_storage.save(curr_list)
-
+#        now = datetime.now()
+#        print("the time: ", now)
     else:
-        sentry_client.user_context({
-            'prev_list': prev_list,
-            'curr_list': curr_list,
-        })
+        # sentry_client.user_context({
+        #     'prev_list': prev_list,
+        #     'curr_list': curr_list,
+        # })
         list_updated = [i for i in curr_list if i not in prev_list]
         if len(list_updated):
             list_untweeted = tweet(list_updated)
             list_to_save = [i for i in curr_list if i not in list_untweeted]
             my_storage.save(list_to_save)
-
+    now = datetime.now()
+    print("the time: ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == '__main__':
     main()
